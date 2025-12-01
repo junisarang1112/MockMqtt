cf904a18484c2393378b1234a7b66eea8d127c85
GitHub Action <action@github.com>
2025-12-01 14:20:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/junisarang1112/mockmqtt/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/junisarang1112/mockmqtt/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.junisarang1112.mockmqtt.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
